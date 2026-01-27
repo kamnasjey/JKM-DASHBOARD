c216b0b16fc8117ddff353fb31f2e7fd8035a414
Kamnasjey <kamnasjey@users.noreply.github.com>
2026-01-27 21:38:41 +0800

    feat: Add Sentry integration

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,40 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
- 
+
 }
 
-export default nextConfig
+import { withSentryConfig } from "@sentry/nextjs";
+
+export default withSentryConfig(
+  nextConfig,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: "jkm-trading",
+    project: "jkm-dashboard",
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+);
+
